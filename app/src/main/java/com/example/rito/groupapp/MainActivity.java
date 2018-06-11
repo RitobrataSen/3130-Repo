e6eb50d9da981812961a957a2a7c47fa015e5846
smitravitz <shane.mitrav@gmail.com>
2018-06-10 21:12:06 -0300

    added activity class MainContentLogin, connected MainActivity via Login button, accepting any username and password currently, to be authenticated later on

--- app/src/main/java/com/example/rito/groupapp/MainActivity.java
+++ app/src/main/java/com/example/rito/groupapp/MainActivity.java
@@ -1,34 +1,51 @@
 package com.example.rito.groupapp;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
 
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
 public class MainActivity extends AppCompatActivity {
+
+    Button loginButton;
+    Button registerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
 
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
+        loginButton = findViewById(R.id.login_button);
+        registerButton = findViewById(R.id.register_button);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, MainContentLogin.class));
+            }
+        });
+
+/*      **default code, for reference:
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+      });
+*/
     }
-
+/*
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -50,6 +67,6 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
+*/
 
 }
---
+++ app/src/main/java/com/example/rito/groupapp/MainContentLogin.java
@@ -0,0 +1,45 @@
+package com.example.rito.groupapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainContentLogin extends AppCompatActivity {
+
+    Button loginButton;
+    EditText userNumber;
+    EditText userPassword;
+    TextView showUserInfo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_main);
+
+        loginButton = findViewById(R.id.login_submit_button);
+        showUserInfo = findViewById(R.id.show_user_info);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                userNumber = findViewById(R.id.user_number);
+                userPassword = findViewById(R.id.user_pw);
+                String username = userNumber.getText().toString();
+                String password = userPassword.getText().toString();
+                String display = "user: " + username + "\npassword: " + password;
+                showUserInfo.setText(display);
+                /*
+                verification steps:
+                if(username.equals() && password.equals()){
+                    startActivity(new Intent(MainContentLogin.this, [featureIncomplete].class));
+                }
+                */
+
+
+            }
+        });
+    }
+}
