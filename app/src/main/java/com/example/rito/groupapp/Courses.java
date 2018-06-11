0dc375ba34b8e8e8d4aa6692c45e1cf1d9f85844
altoria-alter <39277492+altoria-alter@users.noreply.github.com>
2018-06-11 14:35:38 -0300

    Courses updated

---
+++ app/src/main/java/com/example/rito/groupapp/Courses.java
@@ -0,0 +1,68 @@
+package com.example.rito.groupapp;//Project of courses to display the information of the courses
+
+public class Courses {
+    //Variables of courses
+    //Can be extend if more information is needed
+    private int num;
+    private String Cname;
+    private String Sdate;
+    private String Edate;
+    private String Insct;
+
+    //empty constructor
+    public Courses() {
+    }
+
+    //constructor
+    public Courses(int num, String name, String Sd, String Ed, String Ins ){
+        this.num = num;
+        this.Cname = name;
+        this.Sdate = Sd;
+        this.Edate = Ed;
+        this.Insct = Ins;
+    }
+
+    //Set methods
+    //Use to assign the data when we receive information from firebase
+    public void SetNo(int num){
+        this.num = num;
+    }
+
+    public void SetNam(String Nam){
+        this.Cname = Nam;
+    }
+
+    public void SetSd(String date){
+        this.Sdate = date;
+    }
+
+    public void SetEd(String date){
+        this.Edate = date;
+    }
+
+    public void SetIns(String name){
+        this.Insct = name;
+    }
+
+    public int GetNum(){
+        return this.num;
+    }
+
+    //Get methods
+    //For get the information to display on screen
+    public String GetCname(){
+        return this.Cname;
+    }
+
+    public String GetSd(){
+        return this.Sdate;
+    }
+
+    public String GetEd(){
+        return this.Edate;
+    }
+
+    public String GetIns(){
+        return this.Insct;
+    }
+}
