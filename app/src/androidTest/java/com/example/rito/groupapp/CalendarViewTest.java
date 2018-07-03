8f9558644caef0ef89852e5ee389f310cbb4ee93
Dryden Pick <dr286855@dal.ca>
2018-07-03 16:25:32 -0300

    Story 14 -Adding tests, refining UI, preparing methods to add courses to schedule- Dryden and Shane
    Dryden coded while Shane refined.

---
+++ app/src/androidTest/java/com/example/rito/groupapp/CalendarViewTest.java
@@ -0,0 +1,36 @@
+package com.example.rito.groupapp;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.widget.TextView;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class CalendarViewTest {
+
+    @Rule
+    public ActivityTestRule<CalendarView> mActivityTestRule = new ActivityTestRule<>(CalendarView.class);
+
+    //onView clickable button code from stackoverflow user Ads, button with text code from user Ezequiel García:
+    //https://stackoverflow.com/questions/32906881/checking-if-a-button-is-clickable-in-espresso-test-android-studio#32914433
+    @Test
+    public void calendarViewTest() {
+        Courses c = new Courses("CSCI3110", "Computer Science Class", "", "", "", "");
+        c.SetStime("3:30");
+        c.SetEtime("4:30");
+        TextView testTextView = mActivityTestRule.getActivity().monday[1];
+        mActivityTestRule.getActivity().displayCourse(testTextView,c);
+        onView(withId(R.id.m1_body)).check(matches(withText(c.GetCname()+"\n"+c.GetCode()+"\n"+c.GetSt()+"-"+c.GetEt())));
+    }
+}
--- app/src/main/java/com/example/rito/groupapp/CalendarView.java
+++ app/src/main/java/com/example/rito/groupapp/CalendarView.java
@@ -15,59 +15,67 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
+
+
 public class CalendarView extends AppCompatActivity {
 
     public DatabaseReference courseReference;
     ArrayList<String> temp = new ArrayList<>();
     Courses currentCourse;
-    TextView monday;
+    private int courseListSize = 3;
+    public TextView monday[] = new TextView[courseListSize];
+    public TextView tuesday[] = new TextView[courseListSize];
+    public TextView wednesday[] = new TextView[courseListSize];
+    public TextView thursday[] = new TextView[courseListSize];
+    public TextView friday[] = new TextView[courseListSize];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calendar_view);
+        monday[0] = findViewById(R.id.m_body);
+        monday[1] = findViewById(R.id.m1_body);
+        monday[2] = findViewById(R.id.m2_body);
 
-        monday = findViewById(R.id.monday);
+        Courses c = new Courses("CSCI3110", "Computer Science Class", "", "", "", "");
+        c.SetStime("3:30");
+        c.SetEtime("4:30");
 
-        courseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://group-10-9598f.firebaseio.com").child("TERMS").child("201910").child("SUBJECTS").child("CSCI").child("COURSES");
-        courseReference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot snapshot) {
-                if (snapshot.exists()) {
-                    for (DataSnapshot csciCourseSnapshot : snapshot.getChildren()) {
-                        if (csciCourseSnapshot.child("10677").child("course_code").getValue().toString().equals("CSCI1105")) {
-                            //(String num, String name, String Sd, String Ed, String Ins, String max)
-                            //currentCourse = new Courses(csciCourseSnapshot.child("10677").getValue().toString(), csciCourseSnapshot.child("course_name").getValue().toString(),
-                            //                            csciCourseSnapshot.child("start_date").getValue().toString(), csciCourseSnapshot.child("end_date").getValue().toString(),
-                            //                            csciCourseSnapshot.child("instructor").getValue().toString(), csciCourseSnapshot.child("max").getValue().toString());
-                            currentCourse = new Courses();
-                            currentCourse.SetNam(csciCourseSnapshot.child("10677").child("course_name").getValue().toString());
-                            monday.setText(currentCourse.GetCname());
-                            //System.out.println(currentCourse.GetCname());
-                        }
-                        /*
-                        if (csciCourseSnapshot.getValue().toString().equals("")) {
+        displayCourse(monday[1],c);
 
-                        }
-                        if (csciCourseSnapshot.getValue().toString().equals("")) {
-
-                        }
-                        if (csciCourseSnapshot.getValue().toString().equals("")) {
-
-                        }
-                        if (csciCourseSnapshot.getValue().toString().equals("")) {
-
-                        }
-                        */
-                    }
-                }
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-                Log.e("The read failed: ", databaseError.getMessage());
-            }
-        });
+        //courseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://group-10-9598f.firebaseio.com").child("TERMS").child("201910").child("SUBJECTS").child("CSCI").child("COURSES");
+//        courseReference.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot snapshot) {
+//
+//                if (snapshot.exists()) {
+//                    for (DataSnapshot csciCourseSnapshot : snapshot.getChildren()) {
+//                        if (csciCourseSnapshot.child("10677").child("course_code").getValue().toString().equals("CSCI1105")) {
+//                            //(String num, String name, String Sd, String Ed, String Ins, String max)
+//                            //currentCourse = new Courses(csciCourseSnapshot.child("10677").getValue().toString(), csciCourseSnapshot.child("course_name").getValue().toString(),
+//                            //                            csciCourseSnapshot.child("start_date").getValue().toString(), csciCourseSnapshot.child("end_date").getValue().toString(),
+//                            //                            csciCourseSnapshot.child("instructor").getValue().toString(), csciCourseSnapshot.child("max").getValue().toString());
+//                            currentCourse = new Courses();
+//                            currentCourse.SetNam(csciCourseSnapshot.child("10677").child("course_name").getValue().toString());
+//
+//
+//
+//                        }
+           //         }
+           //    }
+//           }
+//
+//            @Override
+//            public void onCancelled(DatabaseError databaseError) {
+//                Log.e("The read failed: ", databaseError.getMessage());
+//            }
+//        });
 
     }
+
+    public void displayCourse(TextView selected, Courses course){
+        selected.setText(course.GetCname()+"\n"+course.GetCode()+"\n"+course.GetSt()+"-"+course.GetEt());
+        //color coded schedule
+        // selected.getBackground().setColorFilter();
+        }
 }
