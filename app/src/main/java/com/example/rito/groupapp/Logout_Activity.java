1a251f61544e8c04729bff7b3e45e17c6b18102c
byevanno <divannodavis@hotmail.com>
2018-07-09 13:13:21 -0300

    finished log out function

---
+++ app/src/main/java/com/example/rito/groupapp/Logout_Activity.java
@@ -0,0 +1,21 @@
+package com.example.rito.groupapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+
+
+public class Logout_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(getBaseContext(),"logout was successful",Toast.LENGTH_LONG).show();
+
+        }
+
+    }
+
