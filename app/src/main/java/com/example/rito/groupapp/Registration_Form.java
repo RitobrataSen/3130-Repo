7aa18c70b5c48e7990d4ccf284dc338797afa28c
smitravitz <shane.mitrav@gmail.com>
2018-07-31 18:00:45 -0300

    [Refactor + Bug fix] Text fields are now found by id and assigned to strings inside of the submit button OnClick(). Input error checking that does not require database entries is now performed before the db reference and datasnapshots are created. Target functionality has not been altered, but now it should be almost impossible to break the activity with inputs and spammy behaviour. @smitravitz, @gobiiv

--- app/src/main/java/com/example/rito/groupapp/Registration_Form.java
+++ app/src/main/java/com/example/rito/groupapp/Registration_Form.java
@@ -35,8 +35,12 @@ import com.google.firebase.database.ValueEventListener;
  * The Registration_Form activity accepts new-user input which is used to
  * populate a User object, and then is pushed to the database.
  *
- * @author  Qu and Divanno
- * @since   07-09-18
+ * Since tag update with structuring refactor/ bug fix.
+ *
+ * @author  Qu, Divanno, Shane
+ * @completed   07-09-18
+ *
+ * @since   07-31-18
  */
 
 public class Registration_Form extends AppCompatActivity {
@@ -44,122 +48,100 @@ public class Registration_Form extends AppCompatActivity {
     private EditText username;
     private EditText password, password2;
     private EditText email;
-    int result;
+
+    private String em;
     private String un;
     private String pw;
-    private String em ;
-    private int em_len = 1;
-    private int un_len = 1;
-    private int pw_len = 1;
+    private String pw2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration__form);
-        Log.d("debug.print","onCreate Registration_Form");
 
-        username = (EditText)findViewById(R.id.user_name);
-        password = (EditText)findViewById(R.id.password);
-        password2 = (EditText)findViewById(R.id.password_validate);
-        email = (EditText)findViewById(R.id.email);
         submit = findViewById(R.id.submit_button);
-        result = -1;
-
         submit.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-              
-                if (password.getText().toString().equals( password2.getText().toString())) {
 
+                email = (EditText) findViewById(R.id.email);
+                username = (EditText) findViewById(R.id.user_name);
+                password = (EditText) findViewById(R.id.password);
+                password2 = (EditText) findViewById(R.id.password_validate);
 
-                    final Context context = getApplicationContext();
-                    final int duration = Toast.LENGTH_SHORT;
-                    DatabaseReference ref;
+                em = email.getText().toString();
+                un = username.getText().toString();
+                pw = password.getText().toString();
+                pw2 = password2.getText().toString();
 
-                    final Database db = new Database();
-                    un = username.getText().toString();
-                    pw = password.getText().toString();
-                    em = email.getText().toString();
+                //empty field checks
+                if (em.isEmpty()) {
+                    Toast.makeText(getApplicationContext(),
+                            "Enter your email first!", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if (un.isEmpty()) {
+                    Toast.makeText(getApplicationContext(),
+                            "Enter your username!", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if (pw.isEmpty()) {
+                    Toast.makeText(getApplicationContext(),
+                            "Enter your password!", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if (pw2.isEmpty()) {
+                    Toast.makeText(getApplicationContext(),
+                            "Confirm your password!", Toast.LENGTH_LONG).show();
+                    return;
+                }
 
-                    ref = db.getDb().getReference("STUDENT");
-                    ref.addListenerForSingleValueEvent(new ValueEventListener() {
-                        @Override
-                        public void onDataChange(DataSnapshot dataSnapshot) {
-                            //check if username is a child of STUDENT
-                            CharSequence text;
-                            boolean exists = false;
+                //matching pw check. db will not be opened until pws match
+                if (!pw.equals(pw2)) {
+                    Toast.makeText(getApplicationContext(), "Enter matching passwords to proceed!", Toast.LENGTH_LONG).show();
+                    return;
+                }
 
-                            if (dataSnapshot.child(un).exists()) {
-                                if(un.length() != 0){
-                                    text = "Sorry, username already exists.";
-                                    Toast toast = Toast.makeText(context, text, duration);
-                                    toast.show();
-                                }
-                                exists = true;
-                            }else if(!(password.getText().toString().equals( password2.getText().toString()))){
-                                text = "Sorry, passwords do not match.";
-                                Toast toast = Toast.makeText(context, text, duration);
-                                toast.show();
-                                exists = true;
-                            } else if(un.length() < un_len ||
-                                    pw.length() < pw_len ||
-                                    em.length() < em_len){
-                                text = "Sorry, 1 or more fields are not correct.";
-                                Toast toast = Toast.makeText(context, text, duration);
-                                toast.show();
-                                exists = true;
-                            }
-                            else {
-                                //verify if email exists
-                                for (DataSnapshot data : dataSnapshot.getChildren()) {
-                                    User u = data.getValue(User.class);
-
-                                    if (u.getEmail().equalsIgnoreCase(em)) {
-                                        text = "Sorry, email already exists.";
-                                        Toast toast = Toast.makeText(context, text, duration);
-                                        toast.show();
+                final Database db = new Database();
+                DatabaseReference ref = db.getDb().getReference("STUDENT");
+                ref.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        boolean exists = false;
+                        if (dataSnapshot.exists()) {
+                            for (DataSnapshot student : dataSnapshot.getChildren()) {
+                                User currentUser = (User) student.getValue(User.class);
+                                if (currentUser != null) {
+                                    if (currentUser.getEmail().equals(em)) {
+                                        Toast.makeText(getApplicationContext(), "This email is already in use!", Toast.LENGTH_LONG).show();
                                         exists = true;
                                         break;
-                                    } else if (u.getUsername().equalsIgnoreCase(un)){
-                                        text = "Sorry, username already exists.";
-                                        Toast toast = Toast.makeText(context, text, duration);
-                                        toast.show();
+                                    }
+                                    else if (currentUser.getUsername().equals(un)) {
+                                        Toast.makeText(getApplicationContext(), "This username is already in use!", Toast.LENGTH_LONG).show();
                                         exists = true;
                                         break;
                                     }
                                 }
+                                else {
+                                    Toast.makeText(getApplicationContext(), "Database error, null student detected!", Toast.LENGTH_LONG).show();
+                                    return;
+                                }
                             }
-                            if (!(exists)) {
-                                db.addUser(em, un, pw);
-                                text = "Success! Added new user!";
-                                Toast toast = Toast.makeText(context, text, duration);
-                                toast.show();
-                                startActivity(new Intent(Registration_Form.this, MainActivity.class));
-                            }
-
                         }
-
-                        @Override
-                        public void onCancelled(DatabaseError databaseError) {
-
+                        if (!exists) {
+                            db.addUser(em, un, pw);
+                            Toast.makeText(getApplicationContext(), "Success! Added new user " + un, Toast.LENGTH_LONG).show();
+                            startActivity(new Intent(Registration_Form.this, MainActivity.class));
                         }
-                    });
+                    }
 
-                }
-
-                if(!(password.getText().toString().equals(password2.getText().toString()))){
-                    Toast.makeText(getBaseContext(), "passwords not the same", Toast.LENGTH_LONG).show();
-                }
-                if( em.length() == 0 ){
-                    Toast.makeText(getBaseContext(), "No email entered", Toast.LENGTH_LONG).show();
-                }else if(un.length() == 0 ){
-                    Toast.makeText(getBaseContext(), "No username entered", Toast.LENGTH_LONG).show();
-                }else if ( pw.length() == 0){
-                    Toast.makeText(getBaseContext(), "No password entered", Toast.LENGTH_LONG).show();
-                }
-
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                    }
+                });
             }
-        });
 
+        });
     }
 }
 
